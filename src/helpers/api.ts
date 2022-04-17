932cc1ae568dd68ea05ef9c62cc7dc4230fd5f1d
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-04-16 19:36:31 -0700

    remove unnecessary method

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -33,8 +33,3 @@ export function updateMerkleRoot() {
     },
   })
 }
-
-export async function getUserCount() {
-  const data = await (await fetch('https://stats.borodutch.com/count')).json()
-  return data.count
-}
